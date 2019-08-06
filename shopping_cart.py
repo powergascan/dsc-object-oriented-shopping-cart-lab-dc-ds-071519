e8c7aab68f9882dd5daa7e82535de602aeebea71
Xin Xue <xx3fe@virginia.edu>
2019-08-06 01:41:10 +0000

    Automatically backed up by Learn

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,7 +3,7 @@ class ShoppingCart(object):
         self.total=total
         self.employee_discount=employee_discount
         self.items=items
-    def add_item(self,items, price):
+    def add_item(self,item, price):
         self.total=+price
         self.items.append(item)
         print(self.total)
