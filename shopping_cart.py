7c0cab824b07cc31ae870c30a9f8c58bd0eb75a0
Xin Xue <xx3fe@virginia.edu>
2019-08-06 01:25:44 +0000

    Automatically backed up by Learn

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,5 @@
 class ShoppingCart(object):
-    def __init__(self, total=0,employee_discount=None, items=[]):
+    def __init__(self, total=0, employee_discount=None, items=[]):
         self.total=total
         self.employee_discount=employee_discount
         self.items=items
